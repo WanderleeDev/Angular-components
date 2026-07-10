d6b3943ae5d1c21eb0e02e74d246eae082e52689
wanderlee <wanderleedev@gmail.com>
2026-07-10 01:47:34 -0500

    refactor(catalog): migrate modal logic from window wrapper to category view

--- src/app/modules/catalog/presentation/components/window/window.ts
+++ src/app/modules/catalog/presentation/components/window/window.ts
@@ -1,35 +1,10 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  input,
-  signal,
-} from '@angular/core';
-import { BtnBaseComponent } from 'app/modules/shared/presentation/components/btn-base/btn-base.component';
-import { ModalCodeComponent } from '../modal-code/modal-code.component';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-window',
-  imports: [BtnBaseComponent, ModalCodeComponent],
   templateUrl: './window.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Window {
   nameComponent = input.required<string>();
-  
-  tailwindPath = input<string | null | undefined>(null);
-  tailwindFiles = input<string[] | null | undefined>(null);
-  
-  cssPath = input<string | null | undefined>(null);
-  cssFiles = input<string[] | null | undefined>(null);
-
-  isTailwindModalOpen = signal(false);
-  isCssModalOpen = signal(false);
-
-  public toggleTailwindModal() {
-    this.isTailwindModalOpen.update(prev => !prev);
-  }
-
-  public toggleCssModal() {
-    this.isCssModalOpen.update(prev => !prev);
-  }
 }
